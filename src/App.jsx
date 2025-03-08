c91764d3378dd045e9abc7cc4f1ff45270e54214
ibrohimova_kamola <ibrohimovakamola19@gmail.com>
2025-03-08 06:20:41 +0500

    Added EnjiResume

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import TiptapEditor from './Editor'
+import React from "react";
+import TiptapEditor from "./Editor";
+import EnjiResume from "./EnjiResume";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <TiptapEditor />
+      <div className="enji-wrapper">
+        <EnjiResume />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
